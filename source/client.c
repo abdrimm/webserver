d25d3d04169f279f3a6614b9a31fa0141eea6933
abdrimm <72120477+abdrimm@users.noreply.github.com>
2020-12-24 17:49:55 +0600

    Update client.c

--- source/client.c
+++ source/client.c
@@ -16,38 +16,36 @@ enum errors {
     ERR_SOCKET,
     ERR_CONNECT
 };
-
 int init_socket(const char *ip, int port);
 int read_string(int fd, char **string);
-void write_string(int fd, char *string);
 
-//  http://127.0.0.1:5000/resource/file.html
+//  http://127.0.0.1:5000/resource/cgi-bin/user?discipline=prog&name=ZZZ
+//  http://127.0.0.1:5000/resource/file.html 
 
 int main(void) {
+    char *error404 = "HTTP/1.1 404\r\n\r\n";
+    char *header_http = "HTTP/1.1";
+    char *http200 = "HTTP/1.1 200\r\n";
     char ch;
     int i = 0;
     int server;
     char request[256];
-    char *arr[6] = {"GET", " ", "HTTP/1.1\r", "Host:", " ", "\r"};
-    char *str[6] = {NULL};
-    int len = 0;
+    char *arr[4] = {"GET ", "  ", " HTTP/1.1\r", "\r\n\r\n"};
+    char *str[7];
     char  *strtok_ans = NULL;
-    char *str1 = "HTTP/1.1 404\n";
-    char *str2 = "HTTP/1.1\n";
     char *string = NULL;
     char *str3 = "http://";
-    char *request_array[4];
+    char *request_array[3];
     fgets(request, 256, stdin);
     while (strcmp(request, "exit\n") != 0) {
         i = 0;
-        while(strstr(request, str3) == NULL && (strcmp(request, "exit\n") != 0)) {
+        while(strstr(request, "http://") == NULL && (strcmp(request, "exit\n") != 0)) {
             puts("Enter correct request");
             fgets(request, 256, stdin);
         }
         strtok_ans = strtok(request, ":/");
-        while((strtok_ans != NULL) && (i != 4)) {
+         while((strtok_ans != NULL) && (i != 4)) {
             request_array[i] = strtok_ans;
-            
             if (i == 2) {
                 strtok_ans = strtok(NULL, "\n");
             } else {
@@ -56,22 +54,18 @@ int main(void) {
             i++;
         }
         arr[1] = request_array[3];
-        arr[4] = request_array[1];
         server = init_socket(request_array[1], atoi(request_array[2]));
-        for(int i = 0; i < 6; i++) {
-            write_string(server, arr[i]);
-            puts(arr[i]);
+        for(int i = 0; i < 4; i++) {
+            write(server, arr[i], strlen(arr[i]) * sizeof(char));
         }
         read_string(server, &str[0]);
-        if ((strcmp(str[0], str1) != 0) && (strcmp(str[0], str2) != 0)) {
+        if ((strcmp(str[0], error404) != 0) && (strcmp(str[0], http200) != 0)) {
             read_string(server, &str[1]);
             read_string(server, &str[2]);
             read_string(server, &str[3]);
             read_string(server, &str[4]);
-            read(server, &len, sizeof(int));
-            read_string(server, &str[5]);
-            read_string(server, &string);
-            printf("%s%s%s%s%s%d%s%s\n", str[0], str[1], str[2], str[3], str[4], len, str[5], string);
+            read_string(server, &str[5]);    
+            printf("%s\n%s\n%s\n%s\n%s\n%s\n", str[0], str[1], str[2], str[3], str[4], str[5]);
         } else {
             printf("%s\n", str[0]);
         }
@@ -79,7 +73,6 @@ int main(void) {
         arr[4] = NULL;
         fgets(request, 256, stdin);
     }
-    server = init_socket(request_array[1], atoi(request_array[2]));
     close(server);
     return OK;
 }
@@ -110,23 +103,13 @@ int init_socket(const char *ip, int port) {
     return server_socket;
 }
 
-void write_string(int fd, char *string) {
-    int i = 0;
-    char ch;
-    while (string[i] != '\0') {
-        write(fd, &string[i], 1);
-        i++;
-    }
-    write(fd, &string[i], 1);
-}
-
 int read_string(int fd, char **string) {
     char *str = NULL;
     char ch;
-    int i = 0, value;
+    int i = 0, status;
     while (1) {
-        value = read(fd, &ch, sizeof(char)); 
-        if(value <= 0) {
+        status = read(fd, &ch, sizeof(char)); 
+        if(status <= 0) {
             break;
         }
         if(ch == '\r') {
@@ -142,5 +125,5 @@ int read_string(int fd, char **string) {
     str = realloc(str, (i + 1) * sizeof(char));
     str[i] = '\0';
     *string = str;
-    return value;
+    return status;
 } 
